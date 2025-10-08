8c074437cf6a58ced7fbc60cd06c63381d066cee
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-10-08 17:01:24 +0200

    Create domain structure for Content, Vocabulary, and Learning

---
+++ lib/deutch_lernen/content.ex
@@ -0,0 +1,13 @@
+defmodule DeutchLernen.Content do
+  @moduledoc """
+  Content domain manages German reading materials and their sentences.
+
+  This domain is responsible for books, sentences, and other reading content
+  that serves as input for German language learning.
+  """
+  use Ash.Domain
+
+  resources do
+    # Resources will be registered here as they are created
+  end
+end
